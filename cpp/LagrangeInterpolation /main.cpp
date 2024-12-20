5f5e5f8598a4ef27bce8a6393daec810c4d7ca69
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-20 13:24:57 +0530

    GT-8 (task) Write detailed unit test cases and refactor code

--- cpp/LagrangeInterpolation /main.cpp
+++ cpp/LagrangeInterpolation /main.cpp
@@ -1,15 +1,21 @@
+#include <gtest/gtest.h>
 #include "../../lib/numericCppExamplesLib/lagrangeInterpolation.h"
 
-int main() {
-  Data<long double> data1{std::vector<long double>{1, 2, 3},
-                          std::vector<long double>{1, 4, 9}};
-  auto poly1 = lagrangePoly(data1);
-  std::cout << poly1(2) << std::endl;
+TEST(LagrangeInterpolationTest, Test1) {
+  Data<long double> data{std::vector<long double>{1, 2, 3},
+                         std::vector<long double>{1, 4, 9}};
+  auto poly = lagrangePoly(data);
+  EXPECT_EQ(poly(2), 4);
+}
 
-  Data<long double> data2{std::vector<long double>{-1, 0, 1, 2},
-                          std::vector<long double>{3, -4, 5, -6}};
-  auto poly2 = lagrangePoly(data2);
-  std::cout << poly2(0.5) << std::endl;
+TEST(LagrangeInterpolationTest, Test2) {
+  Data<long double> data{std::vector<long double>{-1, 0, 1, 2},
+                         std::vector<long double>{3, -4, 5, -6}};
+  auto poly = lagrangePoly(data);
+  EXPECT_EQ(poly(0.5), -0.5);
+}
 
-  return 0;
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
