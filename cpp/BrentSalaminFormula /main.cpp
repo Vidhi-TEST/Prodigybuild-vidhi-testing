ac7bba9c234c857159f898b0bf77aeba1a24fa72
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-24 12:54:34 +0530

    GHT-2 (task) Refactor code and write test cases

--- cpp/BrentSalaminFormula /main.cpp
+++ cpp/BrentSalaminFormula /main.cpp
@@ -1,12 +1,15 @@
+#include <gtest/gtest.h>
 #include <iomanip>
 #include <iostream>
 #include <numeric>
 #include "../../lib/numericCppExamplesLib/brentSalaminFormula.h"
 
-int main() {
+TEST(BrentSalaminFormulaTest, CalculatePi) {
   auto appr_pi = calc_pi<long double>(15);
-  std::cout << std::setprecision(17);
-  std::cout << M_PI << std::endl;
-  std::cout << appr_pi << std::endl;
-  return 0;
+  EXPECT_NEAR(appr_pi, M_PI, 1e-15);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
