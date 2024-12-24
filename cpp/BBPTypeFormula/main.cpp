deede25e1e9a7e952f5eeddc1bc84faff53f0f9d
Vidhi-TEST <test9.vidhi@gmail.com>
2024-12-24 13:00:43 +0530

    GHT-3 (task) Write unit test cases and refactor the code

--- cpp/BBPTypeFormula/main.cpp
+++ cpp/BBPTypeFormula/main.cpp
@@ -1,13 +1,17 @@
-#include <iomanip>
-#include <iostream>
+#include <gtest/gtest.h>
 #include "../../lib/numericCppExamplesLib/bbpTypeFormula.h"
 
-int main() {
-  std::cout << "BBP Type formula implementation" << std::endl;
-  std::cout << "Pi approximation: " << std::setprecision(10)
-            << P(1, 16, 8, std::vector<int>{4, 0, 0, -2, -1, -1, 0, 0}, 10)
-            << std::endl;
-  std::cout << "ln(3) approximation:" << std::setprecision(10)
-            << P(1, 4, 2, std::vector<int>{1, 0}, 10) << std::endl;
-  return 0;
+TEST(BBPTypeFormulaTest, PiApproximation) {
+  double pi_approx = P(1, 16, 8, std::vector<int>{4, 0, 0, -2, -1, -1, 0, 0}, 10);
+  EXPECT_NEAR(pi_approx, 3.1415926535, 0.0000000001);
+}
+
+TEST(BBPTypeFormulaTest, LnApproximation) {
+  double ln_approx = P(1, 4, 2, std::vector<int>{1, 0}, 10);
+  EXPECT_NEAR(ln_approx, 1.0986122887, 0.0000000001);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
